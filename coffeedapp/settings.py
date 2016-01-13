8876341e292867cf6c8a80df1d8e48e002942314
pratapkv2 <pratapkumarv4@gmail.com>
2016-01-13 13:58:43 +0530

    setting.py database changes

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -80,18 +80,29 @@ WSGI_APPLICATION = 'coffeedapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 if ON_HEROKU == True:
-
-    # Parse database configuration from $DATABASE_URL
+# Parse database configuration from $DATABASE_URL
     import dj_database_url
     DATABASES['default'] = dj_database_url.config()
-
-else: 
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(MAIN_DIR, 'db.sqlite3'),
         }
     }
+#if ON_HEROKU == True:
+
+    # Parse database configuration from $DATABASE_URL
+ #   import dj_database_url
+ #   DATABASES['default'] = dj_database_url.config()
+#
+#else: 
+ #   DATABASES = {
+ #       'default': {
+ #           'ENGINE': 'django.db.backends.sqlite3',
+ #           'NAME': os.path.join(MAIN_DIR, 'db.sqlite3'),
+  #      }
+  #  }
 
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
